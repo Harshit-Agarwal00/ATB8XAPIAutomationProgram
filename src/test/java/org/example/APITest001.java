6627486f08659485cc1878714c802bfcc6ac43ac
harshitagarwal <HarshitAgarwal2030@gmail.com>
2024-12-30 13:27:55 +0530

    Added API002

---
+++ src/test/java/org/example/APITest001.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import io.restassured.RestAssured;
+
+public class APITest001 {
+    public static void main(String[] args) {
+        System.out.println("Api Working");
+    }
+}
